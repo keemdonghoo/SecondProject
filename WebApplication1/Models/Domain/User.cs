d90c79d013ce612e17c0031dbbf3d8a8903774a7
jgp904 <jgp904@naver.com>
2023-05-16 14:47:15 +0900

    MovieDbContext 수정

--- WebApplication1/Models/Domain/User.cs
+++ WebApplication1/Models/Domain/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public long UID { get; set; }
+        public string Id { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+
+
     }
 }

a851dbb49f4de249cdc97f2e452bc05f41aaf7b0
jgp904 <jgp904@naver.com>
2023-05-16 11:37:00 +0900

    Interface 구현

--- WebApplication1/Repositories/IWriteRepository.cs
+++ WebApplication1/Repositories/IWriteRepository.cs
@@ -1,23 +1,34 @@
-﻿namespace WebApplication1.Repositories
+﻿using WebApplication1.Models.Domain;
+
+namespace WebApplication1.Repositories
 {
     public interface IWriteRepository
     {
         //현재 상영중인 영화 목록 불러오기
+        Task<IEnumerable<Movie>> GetNowMovie();
+        //특정 영화의 상세정보 불러오기
+        Task<Movie> GetMovieDetail(long id);
 
-        //게시판에 게시글 목록 불러오기
+        //특정 게시글의 댓글 작성하기
+        Task<Review> AddReview(Review review);
+        //특정 게시글의 댓글 불러오기 
+        Task<Review?> GetIdReview(long id);
 
+        //게시글 목록 불러오기
+        Task<IEnumerable<Board?>> GetAllPost();
         //새로운 게시글 생성하기
+        Task<Post> AddPost(Post post);
 
         //특정 ID의 게시글 읽어오기
-
-        //특정 ID의 게시글 수정하기
-
-        //특정 ID의 게시글 삭제하기
-
-        //특정 ID의 게시글 조회수 +1
-
-        //특정 ID의 게시글 좋아요 +1
-
+        Task<Post?> GetPost(long id);
+        //특정 게시글 수정하기
+        Task<Post?> UpdatePost(Post post);
+        //특정 게시글 삭제하기
+        Task<Post?> DeletePost(long id);
+        //특정 게시글 조회수 +1
+        Task<Post?> IncViewCnt();
+        //특정 게시글 좋아요 +1
+        Task<Like> IncLikeCnt();
 
     }
 }
--- WebApplication1/Repositories/WriteRepository.cs
+++ WebApplication1/Repositories/WriteRepository.cs
@@ -1,4 +1,5 @@
 ﻿using WebApplication1.Data;
+using WebApplication1.Models.Domain;
 
 namespace WebApplication1.Repositories
 {
@@ -7,8 +8,63 @@ namespace WebApplication1.Repositories
         private readonly MovieDbContext movieDbContext;
         public WriteRepository(MovieDbContext movieDbContext)
         {
-            Console.WriteLine("WriteRepoitory() 생성");
+            Console.WriteLine("WriteRepoitory() 생성"); 
             this.movieDbContext = movieDbContext;
+        }
+
+        public Task<Post> AddPost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review> AddReview(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post?> DeletePost(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Board?>> GetAllPost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review?> GetIdReview(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovieDetail(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> GetNowMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post?> GetPost(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Like> IncLikeCnt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post?> IncViewCnt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post?> UpdatePost(Post post)
+        {
+            throw new NotImplementedException();
         }
     }
 }

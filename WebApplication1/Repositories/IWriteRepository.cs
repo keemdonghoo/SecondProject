a7d7736b7407590af2db5290c6624739d1ea0a9a
jgp904 <jgp904@naver.com>
2023-05-16 10:12:48 +0900

    Repositories Create

---
+++ WebApplication1/Repositories/IWriteRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication1.Repositories
+{
+    public interface IWriteRepository
+    {
+        //현재 상영중인 영화 목록 불러오기
+
+        //게시판에 게시글 목록 불러오기
+
+        //새로운 게시글 생성하기
+
+        //특정 ID의 게시글 읽어오기
+
+        //특정 ID의 게시글 수정하기
+
+        //특정 ID의 게시글 삭제하기
+
+        //특정 ID의 게시글 조회수 +1
+
+        //특정 ID의 게시글 좋아요 +1
+
+
+    }
+}
---
+++ WebApplication1/Repositories/WriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Data;
+
+namespace WebApplication1.Repositories
+{
+    public class WriteRepository : IWriteRepository
+    {
+        private readonly MovieDbContext movieDbContext;
+        public WriteRepository(MovieDbContext movieDbContext)
+        {
+            Console.WriteLine("WriteRepoitory() 생성");
+            this.movieDbContext = movieDbContext;
+        }
+    }
+}

fc8f3558d3f17540028589f098ac72cebc8d0bc4
jgp904 <jgp904@naver.com>
2023-05-30 11:00:53 +0900

    ㄷㄱ

--- WebApplication1/Migrations/20230526080018_init.Designer.cs
+++ WebApplication1/Migrations/20230526080018_init.Designer.cs
@@ -12,7 +12,7 @@ using TeamProject.Data;
 namespace TeamProject.Migrations
 {
     [DbContext(typeof(MovieDbContext))]
-    [Migration("20230526050315_init")]
+    [Migration("20230526080018_init")]
     partial class init
     {
         /// <inheritdoc />
